e1ccf2f898b3f9413ef63e4881c803f7eae46675
takuminassun <57338551+takuminassun@users.noreply.github.com>
2019-11-15 17:45:00 +0900

    automatic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     image = (message.image.url) ?  `<img src =${message.image.url}>`: "";
     
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id= "${message.id}">
                   <div class="messages__info">
                     <div class="messages__info__talker">
                       ${message.user_name}
@@ -20,8 +20,8 @@ $(function(){
                     ${image}  
                 </div>`
     return html;
-    
   }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -46,4 +46,28 @@ $(function(){
     })
     return false;
   });
+  var reloadMessages = function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id")
+    
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message);
+          $('.message').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function(){
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
 });
